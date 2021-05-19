00aba62a48564c78233840e73fdf586f3d2e9065
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-19 17:34:34 +0200

    terminato primo bonus, tentativo del secondo in atto

--- js/script.js
+++ js/script.js
@@ -10,38 +10,51 @@
 
 var app = new Vue (
     {
-         el:"#root",
-         data:{
-             toDoList:[
-                 {
+        el:"#root",
+        data: {
+            toDoList:[
+                {
                     text:"Fai la spesa",
                     completed:false
-                 },
-                 {
+                },
+                {
                     text:"Compra il latte",
                     completed:false
-                 },
-                 {
+                },
+                {
                     text:"Vai dal meccanico",
                     completed:false
-                 },
-                 {
+                },
+                {
                     text:"Fai la lavatrice",
                     completed:false
-                 }
-             ],
-             newItem:{
-                 completed:false
-             }
-         },
-         methods:{
-            removeItem: function (itemToRemove) {
-                this.toDoList.splice(itemToRemove, 1);
-            },
-            addItem: function () {
-                this.toDoList.push(this.newItem);
-                this.newItem={};
+                }
+            ],
+            listStyle:{
+                style:"none_style"
             }
-         }
+        },
+        methods:{
+            removeItem: function (index) {
+                this.toDoList.splice(index, 1);
+            },
+            addItem: function (event) {
+                const obj = {
+                    text: event.target.value,
+                    completed: false
+                };
+                this.toDoList.push(obj);
+                event.target.value = "";
+            },
+            completedChange: function (item) {
+                item.completed = !item.completed;
+                console.log(item.completed);
+                if(item.completed == true){
+                    this.listStyle.style = "line_style"
+                }else {
+                    this.listStyle.style = "none_style"
+                }
+            },
+        }
     }
 );
